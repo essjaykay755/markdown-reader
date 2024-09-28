d6f0f1aa1e1eac39b51a1ded02f7783e5dfdf857
rocketscience755 <rocketscience755.com>
2024-09-28 14:38:38 +0530

    added vercel analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import MarkdownEditor from "@/components/markdown-editor";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
       {/* <h1 className="text-3xl font-bold mb-4"> Online Markdown Reader</h1> */}
       <MarkdownEditor />
+
+      <Analytics />
     </main>
   );
 }
